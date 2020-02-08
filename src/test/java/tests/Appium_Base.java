c283a298e6d1656f3663fb43dae967ee766c9d94
rslcvz <rslceviz@gmail.com>
2020-02-08 11:51:00 -0500

    Made Base Appium 1

---
+++ src/test/java/tests/Appium_Base.java
@@ -0,0 +1,34 @@
+package tests;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Appium_Base {
+
+    public AppiumDriver driver;
+
+    @Before
+    public void test1() throws InterruptedException {
+
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+
+        desiredCapabilities.setCapability("deviceName", "Pixel_2_API_24");
+        desiredCapabilities.setCapability("platform", "Android");
+        desiredCapabilities.setCapability("platformVersion", "7.0");
+        desiredCapabilities.setCapability("app", System.getProperty("user.dir") + "\\etsy.apk");
+//        desiredCapabilities.setCapability("adbExecTimeout", "20000");
+
+        //"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Android Studio\Android Studio.lnk"   // path for android studio
+        try {
+            driver = new AndroidDriver<>(new URL("http://0.0.0.0:4723/wd/hub"), desiredCapabilities);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/test/java/tests/First_Appium_Test.java
+++ src/test/java/tests/First_Appium_Test.java
@@ -3,43 +3,35 @@ package tests;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.Test;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import javax.print.attribute.standard.PagesPerMinute;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.Key;
 import java.util.concurrent.TimeUnit;
 
-public class First_Appium_Test {
+public class First_Appium_Test extends Appium_Base {
 
-    public AppiumDriver driver;
+//    public AppiumDriver driver;
     @Test
-    public void test1(){
+    public void test2() throws InterruptedException {
 
-        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-
-        desiredCapabilities.setCapability("deviceName", "Pixel_2_API_24");
-        desiredCapabilities.setCapability("platform", "Android");
-        desiredCapabilities.setCapability("platformVersion", "7.0");
-        desiredCapabilities.setCapability("app", System.getProperty("user.dir")+"\\etsy.apk");
-//        desiredCapabilities.setCapability("adbExecTimeout", "20000");
-
-        //"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Android Studio\Android Studio.lnk"   // path for android studio
-        try {
-            driver = new AndroidDriver<>(new URL("http://0.0.0.0:4723/wd/hub"), desiredCapabilities);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-
-//        driver.findElementByXPath("//*[@text='Favorites']").click();
-//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//        driver.findElementById("com.etsy.android:id/search_src_text").sendKeys("shoes");
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 //        driver.findElementById("c856d3e6-f967-4da3-98d2-4b5d955c08a9").click();
         driver.findElementById("com.etsy.android:id/btn_link").click();
 
         driver.findElementById("com.etsy.android:id/edit_username").sendKeys("rslceviz@gmail.com");
         driver.findElementById("com.etsy.android:id/edit_password").sendKeys("691691691r");
         driver.findElementById("com.etsy.android:id/button_signin").click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+       WebElement input = driver.findElementById("com.etsy.android:id/search_src_text");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+      input.sendKeys("shoes");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.getKeyboard().pressKey(Keys.ENTER);
+        Thread.sleep(5000);
+
     }
 }
